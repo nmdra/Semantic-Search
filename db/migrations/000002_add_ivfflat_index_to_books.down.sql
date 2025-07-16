b668f0c9cbc406b218153397da593916b5a56e1e
nmdra <nimendradilshan11@gmail.com>
2025-07-17 03:47:38 +0530

    🐛 fix: Resolve migration script issues for initial schema

--- db/migrations/000002_add_ivfflat_index_to_books.down.sql
+++ db/migrations/000002_add_ivfflat_index_to_books.down.sql
@@ -1,2 +1,1 @@
 DROP INDEX IF EXISTS idx_books_embedding;
-DROP TABLE IF EXISTS books;
--- db/migrations/000002_add_ivfflat_index_to_books.up.sql
+++ db/migrations/000002_add_ivfflat_index_to_books.up.sql
@@ -1,12 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS vector;
-
-CREATE TABLE books (
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  description TEXT NOT NULL,
-  embedding VECTOR(768)
-);
-
 CREATE INDEX IF NOT EXISTS idx_books_embedding
 ON books USING ivfflat (embedding vector_cosine_ops)
 WITH (lists = 100);
